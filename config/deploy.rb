a6008433119f68693c42fe7d41c534de9ec4c814
kou1203 <kou3189@gmail.com>
2022-02-10 20:55:46 +0900

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :pty, true
 
 # シンボリックリンクをはるファイル
 # append :linked_files, "config/master.key"
-append :linked_files, 'config/credentials/production.key'
+append :linked_files, '/var/www/matter/shared/config/credentials/production.key'
 # シンボリックリンクをはるフォルダ
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
 
