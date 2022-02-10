6dfc6c3734bdc68eb03b07d39aa6758deb77c833
kou1203 <kou3189@gmail.com>
2022-02-10 16:19:43 +0900

    .

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,9 @@
 server '160.251.100.189', user: 'kou', roles: %w{app db web}, port: 49155
 
-set :ssh_options, keys: '~/.ssh/conoha_vps'
+# set :ssh_options, keys: '~/.ssh/conoha_vps/id_rsa'
+
+set :ssh_options, {
+  keys: %w(~/.ssh/id_rsa),
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
