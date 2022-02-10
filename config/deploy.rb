e97e2338e97b1f1c48d3213dc1ce689c44a40bf8
kou1203 <kou3189@gmail.com>
2022-02-10 22:15:44 +0900

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :pty, true
 # シンボリックリンクをはるファイル
 # append :linked_files, "config/master.key"
 # append :linked_files, "config/credentials/production.key"
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+set :linked_files, fetch(:linked_files, []).push("config/credentials/production.key")
 # シンボリックリンクをはるフォルダ
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
 
