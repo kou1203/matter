96d6b2ff25607d7cf2c4ad04eff40e108be02a14
kou1203 <kou3189@gmail.com>
2022-02-11 19:46:51 +0900

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :linked_files, fetch(:linked_files, []).push("config/credentials/production.
 # シンボリックリンクをはるフォルダ
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system"
 
-
+set :bundle_gemfile,  "server/Gemfile"
 # 保持するバージョンの個数(※後述)
 set :keep_releases, 3
 
