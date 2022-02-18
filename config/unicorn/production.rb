63e5325499d0b2eb7f232b5908d09e9d0186db17
kou1203 <kou3189@gmail.com>
2022-02-18 23:22:06 +0900

    .

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -5,7 +5,8 @@ $worker  = 2
 #自分のアプリケーション名、currentがつくことに注意。
   $app_dir = "/var/www/matter/current"
 #リクエストを受け取るポート番号を指定。後述
-  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+  # $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+  $listen  = 3000
 #PIDの管理ファイルディレクトリ
   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 #エラーログを吐き出すファイルのディレクトリ
