2417ca805ab6ccd6a8c4be30ba0826b7bab32922
kou1203 <kou3189@gmail.com>
2022-03-10 14:30:20 +0900

    .

--- app/models/dmer.rb
+++ app/models/dmer.rb
@@ -108,87 +108,51 @@ class Dmer < ApplicationRecord
   
         dmer = find_by(store_prop_id:  store_prop.id)
         if store_prop.dmer.present?
-          dmers << {
-            customer_num: row["お申込み番号"],
-            client: row["商流"],
-            user_id: user.id,
-            store_prop_id: store_prop.id,
-            date: row["獲得日"],
-            industry_status: row["業種評価"],
-            status: row["審査ステータス"],
-            status_update: Date.today,
-            share: row["上位店共有日"],
-            shipment: row["キット発送日"],
-            settlementer_id: settlementer_params,
-            settlement: row["初回決済発生日"],
-            settlement_second: row["２回目決済発生日"],
-            picture: row["アクセプタンスアップロード日"],
-            settlement_deadline: row["決済期限"],
-            status_settlement: row["決済ステータス"],
-            status_update_settlement: row["決済ステータス更新日"],
-            payment: row["入金日"],
-            payment_settlement: row["決済入金日"],
-            result_point: row["審査完了日（新規）"],
-            result_point_settlement: row["審査完了日（決済）"],
-            deficiency: row["不備発生日（新規）"],
-            deficiency_settlement: row["不備発生日（決済）"],
-            deficiency_solution: row["不備解消日（新規）"],
-            deficiency_solution_settlement: row["不備解消日（決済）"],
-            deficiency_deadline: row["不備解消期限"],
-            deficiency_remarks: row["不備詳細（新規）"],
-            deficiency_remarks_settlement: row["不備詳細（決済）"],
-            description: row["備考"],
-            profit_new: row["獲得実売"],
-            profit_settlement: row["決済実売"],
-            profit_second_settlement: row["2回目決済実売"],
-            valuation_new: row["獲得評価売"],
-            valuation_settlement: row["決済評価売"],
-            valuation_second_settlement: row["2回目決済評価売"]
-          }
           exist_cnt += 1
         else  
-          dmers << 
-            {
-              customer_num: row["お申込み番号"],
-              client: row["商流"],
-              user_id: user.id,
-              store_prop_id: store_prop.id,
-              date: row["獲得日"],
-              industry_status: row["業種評価"],
-              status: row["審査ステータス"],
-              status_update: Date.today,
-              share: row["上位店共有日"],
-              shipment: row["キット発送日"],
-              settlementer_id: settlementer_params,
-              settlement: row["初回決済発生日"],
-              settlement_second: row["２回目決済発生日"],
-              picture: row["アクセプタンスアップロード日"],
-              settlement_deadline: row["決済期限"],
-              status_settlement: row["決済ステータス"],
-              status_update_settlement: row["決済ステータス更新日"],
-              payment: row["入金日"],
-              payment_settlement: row["決済入金日"],
-              result_point: row["審査完了日（新規）"],
-              result_point_settlement: row["審査完了日（決済）"],
-              deficiency: row["不備発生日（新規）"],
-              deficiency_settlement: row["不備発生日（決済）"],
-              deficiency_solution: row["不備解消日（新規）"],
-              deficiency_solution_settlement: row["不備解消日（決済）"],
-              deficiency_deadline: row["不備解消期限"],
-              deficiency_remarks: row["不備詳細（新規）"],
-              deficiency_remarks_settlement: row["不備詳細（決済）"],
-              description: row["備考"],
-              profit_new: row["獲得実売"],
-              profit_settlement: row["決済実売"],
-              profit_second_settlement: row["2回目決済実売"],
-              valuation_new: row["獲得評価売"],
-              valuation_settlement: row["決済評価売"],
-              valuation_second_settlement: row["2回目決済評価売"]
-            }
           new_cnt += 1
         end
+        dmers << 
+        {
+          customer_num: row["お申込み番号"],
+          client: row["商流"],
+          user_id: user.id,
+          store_prop_id: store_prop.id,
+          date: row["獲得日"],
+          industry_status: row["業種評価"],
+          status: row["審査ステータス"],
+          status_update: Date.today,
+          share: row["上位店共有日"],
+          shipment: row["キット発送日"],
+          settlementer_id: settlementer_params,
+          settlement: row["初回決済発生日"],
+          settlement_second: row["２回目決済発生日"],
+          picture: row["アクセプタンスアップロード日"],
+          settlement_deadline: row["決済期限"],
+          status_settlement: row["決済ステータス"],
+          status_update_settlement: row["決済ステータス更新日"],
+          payment: row["入金日"],
+          payment_settlement: row["決済入金日"],
+          result_point: row["審査完了日（新規）"],
+          result_point_settlement: row["審査完了日（決済）"],
+          deficiency: row["不備発生日（新規）"],
+          deficiency_settlement: row["不備発生日（決済）"],
+          deficiency_solution: row["不備解消日（新規）"],
+          deficiency_solution_settlement: row["不備解消日（決済）"],
+          deficiency_deadline: row["不備解消期限"],
+          deficiency_remarks: row["不備詳細（新規）"],
+          deficiency_remarks_settlement: row["不備詳細（決済）"],
+          description: row["備考"],
+          profit_new: row["獲得実売"],
+          profit_settlement: row["決済実売"],
+          profit_second_settlement: row["2回目決済実売"],
+          valuation_new: row["獲得評価売"],
+          valuation_settlement: row["決済評価売"],
+          valuation_second_settlement: row["2回目決済評価売"]
+        }
       end
       Dmer.upsert_all(dmers)
+      sleep 0.2
       dmers_update = nil
       dmers = nil 
       rows = nil
