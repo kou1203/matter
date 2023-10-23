9dbcccf66e780b558fa57dba99ede31e505eeda9
kou1203 <kou3189@gmail.com>
2023-10-23 19:02:22 +0900

    .
    文字サイズの修正

--- spec/models/dmer_spec.rb
+++ spec/models/dmer_spec.rb
@@ -10,5 +10,28 @@ RSpec.describe Dmer, type: :model do
         expect(@dmer).to be_valid
       end
     end 
+    context '登録できない時' do 
+      it 'user_idがない場合は保存できない' do
+        @dmer.user_id = nil
+        @dmer.valid?
+        expect(@dmer.errors.full_messages).to include "獲得者を入力してください"
+      end 
+      it 'store_prop_idがない場合は保存できない' do 
+        @dmer.store_prop_id = nil 
+        @dmer.valid?
+        expect(@dmer.errors.full_messages).to include "Store propを入力してください"
+      end 
+
+      it '獲得日が入力されていない場合は保存できない' do 
+        @dmer.date = nil 
+        @dmer.valid?
+        expect(@dmer.errors.full_messages).to include "獲得日を入力してください"
+      end 
+      it '獲得日が文字の場合は保存できない' do 
+        @dmer.date = '202310-14'
+        @dmer.valid? 
+        expect(@dmer.errors.full_messages).to include "獲得日を入力してください"
+      end 
+    end 
   end 
 end
