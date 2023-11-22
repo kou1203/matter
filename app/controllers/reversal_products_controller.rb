88312452e445089db4cc036c9414681f6c497d6a
kou1203 <kou3189@gmail.com>
2023-11-22 10:33:30 +0900

    reversal_produce_edit
    戻入案件の編集機能を追加

--- app/controllers/reversal_products_controller.rb
+++ app/controllers/reversal_products_controller.rb
@@ -3,7 +3,17 @@ class ReversalProductsController < ApplicationController
   def index 
     @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
     @reversal_products = ReversalProduct.where(reversal_date: @month.all_month)
+  end 
 
+  def edit 
+    @reversal_product = ReversalProduct.find(params[:id])
+    @users = User.where(base_sub: "キャッシュレス").where.not(position_sub: "99：退職")
+  end 
+  
+  def update 
+    @reversal_product = ReversalProduct.find(params[:id])
+    @reversal_product.update(reversal_product_params)
+    redirect_to reversal_products_path, alert: "#{@reversal_product.store_name}の情報を編集しました。"
   end 
 
   def import 
