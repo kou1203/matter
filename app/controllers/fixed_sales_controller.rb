bd9a4cec9ad6f42094e4ac4cac1aca9f7ec3db50
kou1203 <kou3189@gmail.com>
2023-11-23 12:43:48 +0900

    fixed_sales_index
    固定費の一覧での表示する内容を作成

--- app/controllers/fixed_sales_controller.rb
+++ app/controllers/fixed_sales_controller.rb
@@ -1,6 +1,8 @@
 class FixedSalesController < ApplicationController
 
   def index 
+    @month = params[:month] ? Time.parse(params[:month]) : Date.today
+    @fixed_sales = FixedSale.where(date: @month.all_month)
   end 
-  
+
 end
