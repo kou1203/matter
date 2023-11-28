05a66998cf9e2ea2588fd583150eb4b658dfd574
kou1203 <kou3189@gmail.com>
2023-11-28 11:59:15 +0900

    result_view_test
    resultの結合テスト

---
+++ spec/system/paypays_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'PayPayのViewテスト',type: :system do 
+    before do 
+      @paypay = FactoryBot.create(:paypay)
+      sign_in @paypay.user
+    end 
+    describe '表示テスト' do 
+      before do 
+        visit new_paypay_path(store_id: @paypay.store_prop_id)
+      end 
+      context '新規登録ページ' do 
+        it '登録ボタンが表示されている' do 
+          expect(page.body).to include "登録"
+        end 
+        it '商流を入力する項目が表示される' do 
+          select(value="マックスサポート",from: "paypay[client]")
+          expect(page.body).to include "商流"
+        end 
+
+        it '獲得日を入力するページが表示される' do 
+          expect(page.body).to include "paypay[date]"
+        end 
+
+        it '獲得者を入力するページが表示される' do 
+          expect(page.body).to include "paypay[user_id]"
+        end 
+      end 
+    end 
+end 
--- spec/system/results_spec.rb
+++ spec/system/results_spec.rb
@@ -6,30 +6,31 @@ RSpec.describe '終着入力のテスト', type: :system do
     sign_in @result.user
   end 
   describe '表示テスト' do 
-    context '新規入力画面' do 
-      before do 
-        visit new_result_path(user_id: @result.user_id, date: @result.date)
-      end 
-      it '登録ボタンが表示されている' do 
-        expect(page).to have_button '登録'
-      end 
-    end
     context '新規シフトの場合' do 
       before do 
         visit new_result_path(user_id: @result.user_id, date: @result.date)
       end 
       it '基準値を入力するフォームが表示される' do 
-        select(value='キャッシュレス新規', from: "result[shift]")
         expect(page.body).to include '基準値'
       end 
       it '店舗基準値を入力するフォームが表示される' do 
-        select(value='キャッシュレス新規', from: "result[shift]")
         expect(page.body).to include '店舗別基準値'
       end 
       it '時間別基準値を入力するフォームが表示される' do 
-        select(value='キャッシュレス新規', from: "result[shift]")
         expect(page.body).to include '時間帯別基準値'
       end 
+      it '登録ボタンが表示されている' do 
+        expect(page).to have_button '登録'
+      end 
+      it '入力すると入力すると保存され、切り返しの入力画面へ遷移する' do
+        # 必要な項目を入力
+        fill_in 'result[date]', with: Date.today
+        fill_in 'result[area]', with: 'hoge'
+        select(value='キャッシュレス新規', from: 'result[shift]')
+        expect{find('input[name=commit]').click}.to change { Result.count }.by(1)
+        # 必要な項目が入力された状態で登録ボタンを押したら切り返しの入力項目へ移動するか
+        expect(current_path).to eq(result_result_cashes_new_path(@result.id + 1))
+      end
     end 
   end 
 end 
