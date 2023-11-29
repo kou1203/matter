90234aab2ab0ec366856613f272ac25e20b72320
kou1203 <kou3189@gmail.com>
2023-11-29 11:05:47 +0900

    d専売人員の商流編集と削除する機能追加

--- app/controllers/select_columns_controller.rb
+++ app/controllers/select_columns_controller.rb
@@ -1,9 +1,4 @@
 class SelectColumnsController < ApplicationController
-
-  def index 
-    
-  end 
-
   def new 
     @category = params[:category]
     @select_column = SelectColumn.new 
@@ -18,6 +13,24 @@ class SelectColumnsController < ApplicationController
     else 
       render :new 
     end 
+  end 
+
+  def edit 
+    @select_column = SelectColumn.find(params[:id])
+    session[:previous_url] = request.referer
+  end 
+  
+  def update 
+    @select_column = SelectColumn.find(params[:id])
+    @select_column.update(select_column_params)
+    redirect_to session[:previous_url],alert: "#{@select_column.category}の名前を修正しました。"
+    
+  end 
+  
+  def destroy 
+    @select_column = SelectColumn.find(params[:id])
+    @select_column.destroy
+    redirect_to request.referer,alert: "#{@select_column.category}の#{@select_column.name}を削除しました。"
   end 
 
   private 
