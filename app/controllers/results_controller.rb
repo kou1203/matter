4f8f80341f46d08d69cca3c4b7cecc5330d4e1c9
kou1203 <kou3189@gmail.com>
2025-07-27 13:04:20 +0900

    消化シフトではなく予定シフトでaveを出すよう修正

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -337,7 +337,7 @@ class ResultsController < ApplicationController
   end 
     
   def weekly_fin # 週間基準値 前月基準値
-    @shift_digestion_new = @results.where(shift: "キャッシュレス新規").length
+    @shifts_len = Shift.where(user_id: @user.id).where(start_time: @month.all_month).where(shift: "キャッシュレス新規").length
     # 前月の終着
     @results_prev = Result.includes(:user).where(user_id: @user.id).where(date: @month.prev_month.all_month)
     @prev_month = @month.prev_month
